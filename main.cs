00ea10ef363fab98e2aa158998b7bfae08558d31
Jeff <jeckman03@gmail.com>
2021-10-25 18:24:03 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+
+		// Welcome the user to the app
+		Greeting();
+
+		// As if they are a boy or a girl
+
+		// Generate a random name
+
+		// Ask if they want to generate again, go back, or quit
+
+
+    Console.ReadLine();
+  }
+
+	public static void Greeting()
+	{
+		Console.WriteLine($"WELCOME TO THE RANDOM NAME GENERATOR APP");
+	}
+
+}
